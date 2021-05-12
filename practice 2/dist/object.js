73da2edb53af6136746fc975c3d40ce67f366199
Muhammad Maaz Uddin <maaznadeem246@gmail.com>
2021-05-12 11:18:35 -0700

    Practice while watcing the freecodecamp typescript Video

---
+++ practice 2/dist/object.js
@@ -0,0 +1,16 @@
+"use strict";
+var user = {
+    name: "Monster",
+    age: 30,
+    getMessage: function () {
+        return "hello" + user.name;
+    }
+};
+var user2 = {
+    name: "jake",
+    getMessage: function () {
+        return "hello" + user.name;
+    }
+};
+console.log(user.name);
+//# sourceMappingURL=object.js.map
---
+++ practice 2/src/object.ts
@@ -0,0 +1,22 @@
+interface User {
+    name: string,
+    age?: number,
+    getMessage():string
+}
+
+const user:User ={
+    name:"Monster",
+    age:30,
+    getMessage(){
+        return "hello"+user.name
+    }
+}   
+
+const user2:User = {    
+    name:"jake",
+    getMessage(){
+        return "hello"+user.name
+    }
+}
+
+console.log(user.name)
